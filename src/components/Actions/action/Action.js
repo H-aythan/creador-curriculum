23a946846c252902312c1811246aa80d898cf314
Andres A. Gallardo Ysla <agallardoysla@gmail.com>
2021-10-22 08:28:08 -0500

    refactoring action.js

--- src/components/Actions/action/Action.js
+++ src/components/Actions/action/Action.js
@@ -1,43 +1,62 @@
-import React, { Component } from 'react';
-import './Action.scss';
-import ActionIntroduction from '../action-step-introduction/ActionIntroduction'
-import ActionStepSelection from '../action-step-selection/ActionSelection'
-import ActionFilling from '../action-step-filling/ActionFilling'
-import Dashboard from '../../Dashboard/DashboardMain/DashboardMain'
-import { Redirect } from "react-router-dom";
-class Action extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      isAuthShowed: false,
-    }
-    this.authBtnHandler = this.authBtnHandler.bind(this)
+import React, { useEffect, useState } from "react";
+import "./Action.scss";
+import ActionIntroduction from "../action-step-introduction/ActionIntroduction";
+import ActionStepSelection from "../action-step-selection/ActionSelection";
+import ActionFilling from "../action-step-filling/ActionFilling";
+
+function Action({
+  goThirdStep,
+  logout,
+  user,
+  currentStep,
+  setCurrentStep,
+  handlePreviewToggle,
+  values,
+  handleDelete,
+  progress,
+  handleInputs,
+}) {
+  const [isAuthShowed, setAuthShowed] = useState(false);
+
+  const authBtnHandler = () =>
+    user == null ? setAuthShowed(!isAuthShowed) : setAuthShowed(false);
+
+  switch (currentStep) {
+    case "Introduction":
+      return (
+        <ActionIntroduction
+          goThirdStep={goThirdStep}
+          isAuthShowed={isAuthShowed}
+          authBtnHandler={authBtnHandler}
+          logout={logout}
+          user={user}
+        />
+      );
+    case "Template Selection":
+      return (
+        <ActionStepSelection
+          setCurrentStep={setCurrentStep}
+          isAuthShowed={isAuthShowed}
+          authBtnHandler={authBtnHandler}
+          logout={logout}
+          user={user}
+          handlePreviewToggle={handlePreviewToggle}
+        />
+      );
+    case "Adding Data":
+      return (
+        <ActionFilling
+          values={values}
+          logout={logout}
+          user={user}
+          handleDelete={handleDelete}
+          progress={progress}
+          handleInputs={handleInputs}
+        />
+      );
+    default:
+      return <ActionIntroduction />;
   }
-  authBtnHandler() {
-    if (this.props.user == null) {
-      this.setState((prevState, props) => ({
-        isAuthShowed: prevState.isAuthShowed ? false : true
-      }));
-    } else if (this.props.user != null) {
-      this.setState({
-        isAuthShowed: false
-      })
-    }
-  }
-  /// This  class have nextStep passed to it in props so we be able to navigate between steps 
-  // Note Step state is handled in parent to render the right components
-  render() {
-    // Checking which step is passed to the action wrapper and render the right component 
-    switch (this.props.currentStep) {
-      case "Introduction":
-        return (<ActionIntroduction goThirdStep={this.props.goThirdStep} isAuthShowed={this.state.isAuthShowed} authBtnHandler={this.authBtnHandler} logout={this.props.logout} user={this.props.user} />);
-      case "Template Selection":
-        return (<ActionStepSelection setCurrentStep={this.props.setCurrentStep} isAuthShowed={this.state.isAuthShowed} authBtnHandler={this.authBtnHandler} logout={this.props.logout} user={this.props.user} handlePreviewToggle={this.props.handlePreviewToggle} />);
-      case "Adding Data":
-        return (<ActionFilling values={this.props.values} logout={this.props.logout} user={this.props.user} handleDelete={this.props.handleDelete} progress={this.props.progress} handleInputs={this.props.handleInputs} />);
-      default:
-        return (<ActionIntroduction />);
-    }
-  };
 }
+
 export default Action;
